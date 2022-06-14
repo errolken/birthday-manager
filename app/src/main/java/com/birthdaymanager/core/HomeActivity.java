d95dca907fdf9b0ce25e4656fee5fdd11305c18d
errol <errolken2008@gmail.com>
2022-06-14 19:17:51 +0530

    Added Home Page

---
+++ app/src/main/java/com/birthdaymanager/core/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.birthdaymanager.core;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
